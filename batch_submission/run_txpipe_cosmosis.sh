0180276017c95d117cddf8120767bf37ea327e50
Chihway Chang <chihway54@gmail.com>
2020-02-28 11:21:04 -0800

    batch draft

---
+++ batch_submission/run_txpipe_cosmosis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash   
+#SBATCH -N 2
+#SBATCH --qos=regular
+#SBATCH --time=15:00:00
+#SBATCH --job-name=TXPipe_Test
+#SBATCH --image=docker:joezuntz/txpipe_cosmosis_firecrown:latest
+#SBATCH --license=SCRATCH
+#SBATCH --constraint=knl
+#SBATCH --mail-user=chihway@kicp.uchicago.edu
+
+export OMP_NUM_THREADS=1
+
+source setup-firecrown
+
+
+srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown compute /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml 
+
+srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown run-emcee /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml
+
+
