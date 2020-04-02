73c7be91ce96829536657965cd08b545df56cc1e
Chihway Chang <chihway54@gmail.com>
2020-04-02 13:42:33 -0700

    cosmosis standalone works

--- batch_submission/run_txpipe_cosmosis.sh
+++ batch_submission/run_txpipe_cosmosis.sh
@@ -1,20 +1,31 @@
 #!/bin/bash   
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH --qos=regular
-#SBATCH --time=15:00:00
-#SBATCH --job-name=TXPipe_Test
-#SBATCH --image=docker:joezuntz/txpipe_cosmosis_firecrown:latest
+#SBATCH --time=10:00:00
+#SBATCH --job-name=cosmodc2_test
+##SBATCH --image=docker:joezuntz/txpipe_cosmosis_firecrown:latest
 #SBATCH --license=SCRATCH
-#SBATCH --constraint=knl
+#SBATCH --constraint=haswell
+## knl
 #SBATCH --mail-user=chihway@kicp.uchicago.edu
 
 export OMP_NUM_THREADS=1
 
-source setup-firecrown
+#source setup-firecrown
+
+module load python
+pip uninstall parsl
+pip uninstall ceci
+pip uninstall pyccl
+
+#export PYTHONPATH=$PYTHONPATH:/global/homes/c/chihway/TXPipe:/global/homes/c/chihway/TJPCov
+#shifter --volume=/global/homes/c/chihway/cosmosis:/opt/cosmosis --volume=/global/homes/c/chihway/FireCrown/:/opt/firecrown --volume=/global/homes/c/chihway/TXPipe:/opt/txpipe --image=joezuntz/txpipe_cosmosis_firecrown -- bash
 
 
-srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown compute /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml 
+#firecrown compute /global/homes/c/chihway/txpipe-cosmodc2/firecrown_config/cosmodc2_firecrown_real.yaml
 
-srun -n  64 shifter --volume=/global/cscratch1/sd/elp25/cosmosis:/opt/cosmosis --volume /global/cscratch1/sd/elp25/FireCrown/:/opt/firecrown --volume /global/cscratch1/sd/elp25/TXPipe:/opt/txpipe firecrown run-emcee /global/cscratch1/sd/elp25/FireCrown/examples/des_y1_3x2pt/des_y1_3x2pt.yaml
+#srun -n  64 shifter --volume=/global/homes/c/chihway/cosmosis:/opt/cosmosis --volume /global/homes/c/chihway/FireCrown/:/opt/firecrown --volume /global/homes/c/chihway/TXPipe:/opt/txpipe firecrown run-emcee /global/homes/c/chihway/txpipe-cosmodc2/firecrown_config/cosmodc2_firecrown_real.yaml
+
+srun -n  48 shifter --volume=/global/homes/c/chihway/cosmosis:/opt/cosmosis --volume=/global/homes/c/chihway/FireCrown/:/opt/firecrown   --volume=/global/homes/c/chihway/TXPipe:/opt/txpipe --image=joezuntz/txpipe_cosmosis_firecrown firecrown run-emcee /global/homes/c/chihway/txpipe-cosmodc2/firecrown_config/cosmodc2_firecrown_real.yaml 
 
 
