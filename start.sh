123275af1e21d75ba01b2ba8b33791e1801c6fb8
Chihway Chang <chihway54@gmail.com>
2020-04-14 07:23:40 -0700

    multinest setup that runs and outputs chains

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ pip uninstall parsl
 pip uninstall ceci
 pip uninstall pyccl
 
-export PYTHONPATH=$PYTHONPATH:/global/homes/c/chihway/TXPipe_dc2/TXPipe:/global/homes/c/chihway/TJPCov
+#export PYTHONPATH=$PYTHONPATH:/global/homes/c/chihway/TXPipe:/global/homes/c/chihway/TJPCov
 #shifter --image docker:joezuntz/txpipe bash
-shifter --volume=$PWD/cosmosis:/opt/cosmosis --volume $PWD/FireCrown/:/opt/firecrown --volume $PWD/TXPipe:/opt/txpipe --image=joezuntz/txpipe_cosmosis_firecrown -- bash
+#shifter --volume=$PWD/cosmosis:/opt/cosmosis --volume $PWD/FireCrown/:/opt/firecrown --volume $PWD/TXPipe:/opt/txpipe --image=joezuntz/txpipe_cosmosis_firecrown -- bash
 
-#module load python
-#pip uninstall parsl
-#pip uninstall ceci
+shifter --image=joezuntz/txpipe-firecrown -V $PWD/FireCrown:/opt/firecrown -V $PWD/TXPipe:/opt/txpipe bash
+
+export PYTHONPATH=/global/homes/c/chihway/TJPCov:$PYTHONPATH 
+
+
 
