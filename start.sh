0cc092fd09b23c8ba17b039e0355880a8bf97d00
Chihway Chang <chihway54@gmail.com>
2020-05-08 14:24:44 -0700

    upload scale cut

--- start.sh
+++ start.sh
@@ -4,14 +4,17 @@ module load python
 pip uninstall parsl
 pip uninstall ceci
 pip uninstall pyccl
+pip uninstall firecrown
 
 #export PYTHONPATH=$PYTHONPATH:/global/homes/c/chihway/TXPipe:/global/homes/c/chihway/TJPCov
 #shifter --image docker:joezuntz/txpipe bash
 #shifter --volume=$PWD/cosmosis:/opt/cosmosis --volume $PWD/FireCrown/:/opt/firecrown --volume $PWD/TXPipe:/opt/txpipe --image=joezuntz/txpipe_cosmosis_firecrown -- bash
 
-shifter --image=joezuntz/txpipe-firecrown -V $PWD/FireCrown:/opt/firecrown -V $PWD/TXPipe:/opt/txpipe bash
+#shifter --image=joezuntz/txpipe-firecrown -V $PWD/FireCrown:/opt/firecrown -V $PWD/TXPipe:/opt/txpipe bash
 
-export PYTHONPATH=/global/homes/c/chihway/TJPCov:$PYTHONPATH 
+shifter --image=joezuntz/txpipe  bash
+
+export PYTHONPATH=/global/homes/c/chihway/TJPCov:/global/homes/c/chihway/FlexZPipe:$PYTHONPATH 
 
 
 
